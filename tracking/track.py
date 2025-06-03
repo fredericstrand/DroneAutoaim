c69f10530ad8a389d30c1f8ed6fef2f8c32f5aaa
fredericstrand <strandfrederic@outlook.com>
2025-06-03 18:04:48 +0200

    removing arudion

--- tracking/track.py
+++ tracking/track.py
@@ -1,19 +1,13 @@
 from ultralytics import YOLO
 import cv2 as cv
-import serial
-import time
-
-# model path: ../pretraning/models/weights/best.pt
+from gpiozero import AngularServo
+from time import sleep
 
 model = YOLO('yolov8s.pt')
-
-# Open webcam
 cap = cv.VideoCapture(0)
+servo_x = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
+servo_y = AngularServo(17, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
-arduino = serial.Serial('/dev/ttyACM0', 9600)
-time.sleep(2)
-
-# Frame size
 FRAME_WIDTH = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 FRAME_HEIGHT = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
@@ -25,26 +19,20 @@ while True:
     results = model.predict(source=frame, stream=True)
 
     for result in results:
-        boxes = result.boxes
-        for box in boxes:
+        for box in result.boxes:
             x1, y1, x2, y2 = map(int, box.xyxy[0])
-            conf = float(box.conf[0])
-            cls = int(box.cls[0])
-            label = f'{model.names[cls]} {conf:.2f}'
-
-            # Compute the center
             box_cx = (x1 + x2) // 2
             box_cy = (y1 + y2) // 2
 
-            x_angle = int((box_cx / FRAME_WIDTH) * 180)
-            y_angle = int((box_cy / FRAME_HEIGHT) * 180)
+            x_angle = int((box_cx / FRAME_WIDTH) * 180) - 90
+            y_angle = int((box_cy / FRAME_HEIGHT) * 180) - 90
 
-            arduino.write(f"{x_angle},{y_angle}\n".encode())
+            servo_x.angle = x_angle
+            servo_y.angle = y_angle
 
             cv.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            label = f'{model.names[int(box.cls[0])]} {float(box.conf[0]):.2f}'
             cv.putText(frame, label, (x1, y1 - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-
-            print(f'{label}: ({x1}, {y1}), ({x2}, {y2}) , Confidence: {conf:.2f}')
 
     cv.imshow('YOLOv8 Tracking', frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
@@ -52,4 +40,3 @@ while True:
 
 cap.release()
 cv.destroyAllWindows()
-
